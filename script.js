db9bebf8927b8f2006400397ea1c9769be63686e
maiacabrera59-oss <maiacabrera59@gmail.com>
2026-05-11 18:57:27 -0300

    cambios en JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 alert ("Bienvenidos");
 console.log("¡Hola!"); 
 
-document.querySelector("h1").innerHTML = "¡CAMBIAMOS EL TÍTULO!"; //camniamos el titulo del h1 a través de js
+document.querySelector("h1").innerHTML = "¡CAMBIAMOS EL TÍTULO!"; //cambiamos el titulo del h1 a través de js
 
 document.querySelector('#boton').onclick = function(){
     document.body.style.backgroundColor = "lightblue"; //cambiamos el fondo a través de js
